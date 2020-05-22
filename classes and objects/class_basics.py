d49c6d422328ffdcf77b4371783c0c0c06c67744
Shankar1528 <42579464+Shankar1528@users.noreply.github.com>
2020-05-22 05:17:28 -0400

    Create class_basics.py

---
+++ classes and objects/class_basics.py
@@ -0,0 +1,19 @@
+class Dog:  
+      
+    # A simple class 
+    # attribute 
+    attr1 = "mamal"
+    attr2 = "dog"
+  
+    # A sample method   
+    def fun(self):  
+        print("I'm a", self.attr1) 
+        print("I'm a", self.attr2) 
+  
+# Object instantiation 
+Rodger = Dog() 
+  
+# Accessing class attributes 
+# and method through objects 
+print(Rodger.attr1) 
+Rodger.fun() 
