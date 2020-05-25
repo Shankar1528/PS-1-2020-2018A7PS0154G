191dc0bd4f65ca7738064b3efedde1f199876b7e
Shankar1528 <42579464+Shankar1528@users.noreply.github.com>
2020-05-25 06:40:14 -0400

    Update and rename oops_parrot_1 to oops_parrot_1.py

--- classes and objects/oops_parrot_1.py
+++ classes and objects/oops_parrot_1.py
@@ -2,12 +2,11 @@ class Parrot:
 
     # class attribute
     species = "bird"
-
+    
     # instance attribute
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
 # instantiate the Parrot class
 blu = Parrot("Blu", 10)
 woo = Parrot("Woo", 15)
