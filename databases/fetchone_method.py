3f189044da6cf478c3899cb70e76a7091584788b
Shankar1528 <42579464+Shankar1528@users.noreply.github.com>
2020-05-29 05:49:41 -0400

    Update fetchone_method.py

--- databases/fetchone_method.py
+++ databases/fetchone_method.py
@@ -10,10 +10,12 @@ mydb = mysql.connector.connect(
   database="mydatabase"
 )
 
+
 mycursor = mydb.cursor()
 
 mycursor.execute("SELECT * FROM customers")
 
 myresult = mycursor.fetchone()
+
 
 print(myresult) 
