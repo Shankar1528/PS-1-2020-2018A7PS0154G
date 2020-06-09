86e636c51bdafb693080e0bafa6bafc867de4e55
Shankar1528 <42579464+Shankar1528@users.noreply.github.com>
2020-06-09 03:01:01 -0400

    Update Modifying Array Values.py

--- numpy/iterating over array/Modifying Array Values.py
+++ numpy/iterating over array/Modifying Array Values.py
@@ -17,9 +17,11 @@ import numpy as np
 a = np.arange(0,60,5) 
 a = a.reshape(3,4) 
 
+
 print 'Original array is:' 
 print a 
 print '\n'  
+
 
 print 'Modified array is:' 
 for x in np.nditer(a, flags = ['external_loop'], order = 'F'):
