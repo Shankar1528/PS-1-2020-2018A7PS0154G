80536df868396ecf1cf62e0d05014941c13e546b
Shankar1528 <42579464+Shankar1528@users.noreply.github.com>
2020-06-17 03:37:45 -0400

    Update multiplots.py

--- Matplotlib/multiplots.py
+++ Matplotlib/multiplots.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 # plot a line, implicitly creating a subplot(111)
 plt.plot([1,2,3])
 # now create a subplot which represents the top plot of a grid with 2 rows and 1 column.
-#Since this subplot will overlap the first, the plot (and its axes) previously 
-created, will be removed
+#Since this subplot will overlap the first, the plot (and its axes) previously
+#created, will be removed
 plt.subplot(211)
 plt.plot(range(12))
 plt.subplot(212, facecolor='y') # creates 2nd subplot with yellow background
@@ -17,7 +17,6 @@ ax1 = fig.add_subplot(111)
 ax1.plot([1,2,3])
 ax2 = fig.add_subplot(221, facecolor='y')
 ax2.plot([1,2,3])
-
 
 
 import matplotlib.pyplot as plt
